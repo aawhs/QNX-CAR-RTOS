e2c4e007946f4b3e0028239227607fc9cac39f1d
superthunder <alexander.wolfe.10@googlemail.com>
2021-11-06 23:27:29 -0400

    processed data files

--- data/parse-dataset.py
+++ data/parse-dataset.py
@@ -1,3 +1,25 @@
+import csv
+
 dataset_name='dataset.csv'
+output_prefix='dataset-'
+
+# Variables that we want split out into their own files
+vars_of_interest = ['Fuel_consumption', 'Engine_coolant_temperature', 'Engine_speed', 'Current_Gear', 'Vehicle_speed']
 
 # Need to split the CSV into one file per column
+with open(dataset_name, 'r') as f:
+    reader = csv.DictReader(f)
+
+    rows = [x for x in reader]
+    columns = [x for x in rows[0].keys() if x in vars_of_interest]
+
+print(columns)
+
+for col in columns:
+    fname = output_prefix+col+'.txt'
+    print('Writing to ', fname)
+    with open(fname, 'w', newline='') as f:
+        writer = csv.writer(f)
+        for row in rows:
+            line = [row[col]]
+            writer.writerow(line)
