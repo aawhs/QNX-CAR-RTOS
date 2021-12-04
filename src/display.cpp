8235564372f4f6f4428edba227a24a8c2dd777bd
superthunder <alexander.wolfe.10@googlemail.com>
2021-12-04 18:55:12 -0500

    typos

--- src/display.cpp
+++ src/display.cpp
@@ -19,7 +19,7 @@ void displayFunc(){
 
 	while (1) {
 
-		std::cout<<"full consumption | engine speed (RPM) | Engine Coolant Temperature | Current Gear | Vehicle Speed"<<std::endl;
+		std::cout<<"Fuel Consumption | Engine Speed (RPM) | Engine Coolant Temperature | Current Gear | Vehicle Speed"<<std::endl;
 		for(int i = 0; i < 5; i++){
 			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 			std::cout<<appcore::readSharedMemFloat(5)<<"\t \t \t"<<appcore::readSharedMemFloat(6)<<"\t \t"<<appcore::readSharedMemFloat(7)<<"\t \t \t \t"<<appcore::readSharedMemFloat(8)<<"\t \t"<<appcore::readSharedMemFloat(9)<<std::endl;
