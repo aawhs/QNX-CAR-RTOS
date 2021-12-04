859d32b08a956a860e24f8a5e48010523eb98146
superthunder <alexander.wolfe.10@googlemail.com>
2021-12-04 14:30:12 -0500

    add implementation notes

--- src/COEN320-Project.cpp
+++ src/COEN320-Project.cpp
@@ -11,35 +11,8 @@
 #include "appcore.hpp"
 
 
-//The messaging system is essentially a 5 queue fan out / 5 queue fan in
-
-//Struct for messages from Command to Producers
-typedef struct _command_msg{
-    msg_header_t hdr;
-    int update_freq;
-} command_msg_t;
-
-//Struct for messages from Producers to Display
-typedef struct _producer_msg{
-    msg_header_t hdr;
-    float value;
-} producer_msg_t;
-
-//Command input to producers attach points
-#define ATTACH_INPUT_TO_PRODUCER_1 "c2p_1"
-#define ATTACH_INPUT_TO_PRODUCER_2 "c2p_2"
-#define ATTACH_INPUT_TO_PRODUCER_3 "c2p_3"
-#define ATTACH_INPUT_TO_PRODUCER_4 "c2p_4"
-#define ATTACH_INPUT_TO_PRODUCER_5 "c2p_5"
-
-
-//Producers to display attach points
-#define ATTACH_PRODUCER_1_TO_DISPLAY "p2d_1"
-#define ATTACH_PRODUCER_2_TO_DISPLAY "p2d_2"
-#define ATTACH_PRODUCER_3_TO_DISPLAY "p2d_3"
-#define ATTACH_PRODUCER_4_TO_DISPLAY "p2d_4"
-#define ATTACH_PRODUCER_5_TO_DISPLAY "p2d_5"
-
+//The IPC system is essentially a 5 variable fan out / 5 variable fan in
+//But implemented with shared memory
 
 
 int main() {
@@ -48,32 +21,21 @@ int main() {
 	appcore ac; //create instance so that starting time is initialized
 	std::cout << "Initial time: " << ac.getElapsedTimeSeconds() << std::endl;
 
-	//Does indeed show 5
-	sleep(5);
-	std::cout << "New time: " << ac.getElapsedTimeSeconds() << std::endl;
 
-	sleep(5);
-	std::cout << "New time: " << ac.getElapsedTimeSeconds() << std::endl;
-
-	sleep(5);
-	std::cout << "New time: " << ac.getElapsedTimeSeconds() << std::endl;
 
 
 	//IPC for command input task -> producer tasks
 
-
-
-
 	//IPC for producer tasks -> consumer task
 
 
+	//Create the command processor instance
 
 
+	//Create the 5 producer instances
 
 
-
-
-
+	//Create the consumer (display) instance
 
 
 
