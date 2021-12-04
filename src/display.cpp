0718ceb471dcc5ccf02097ee72eafb9e9860627d
Tianyang-Jin11 <77936386+Tianyang-Jin11@users.noreply.github.com>
2021-12-04 18:48:51 -0500

    Update display.cpp

--- src/display.cpp
+++ src/display.cpp
@@ -5,5 +5,24 @@
  *      Author: Tianyang
  */
 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <chrono>
+#include <thread>
 #include "display.hpp"
+#include <iostream>
+
+
+void displayFunc(){
+
+	while (1) {
+
+		std::cout<<"full consumption | engine speed (RPM) | Engine Coolant Temperature | Current Gear | Vehicle Speed"<<std::endl;
+		for(int i = 0; i < 5; i++){
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+			std::cout<<appcore::readSharedMemFloat(5)<<"\t \t \t"<<appcore::readSharedMemFloat(6)<<"\t \t"<<appcore::readSharedMemFloat(7)<<"\t \t \t \t"<<appcore::readSharedMemFloat(8)<<"\t \t"<<appcore::readSharedMemFloat(9)<<std::endl;
+		}
+	}
+}
