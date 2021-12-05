ef177a6d6a375bafd146fa587a02a66b3825405b
superthunder <alexander.wolfe.10@googlemail.com>
2021-12-04 21:18:07 -0500

    improve cli output

--- src/display.cpp
+++ src/display.cpp
@@ -19,7 +19,7 @@ void displayFunc(){
 
 	while (1) {
 
-		std::cout<<"Time | Fuel Consumption | Engine Speed (RPM) | Engine Coolant Temperature | Current Gear | Vehicle Speed"<<std::endl;
+		std::cout<<std::endl<<"Time | Fuel Consumption | Engine Speed (RPM) | Engine Coolant Temperature | Current Gear | Vehicle Speed"<<std::endl;
 		for(int i = 0; i < 15; i++){
 			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 			std::cout<<appcore::getElapsedTimeSeconds() << "\t" << appcore::readSharedMemFloat(5)<<"\t \t \t"<<appcore::readSharedMemFloat(6)<<"\t \t"<<appcore::readSharedMemFloat(7)<<"\t \t \t \t"<<appcore::readSharedMemFloat(8)<<"\t \t"<<appcore::readSharedMemFloat(9)<<std::endl;
