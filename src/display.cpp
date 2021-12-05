d8e4d8cc47fea939560cfc86e155df10fa9e1389
superthunder <alexander.wolfe.10@googlemail.com>
2021-12-04 22:50:11 -0500

    better output

--- src/display.cpp
+++ src/display.cpp
@@ -19,7 +19,13 @@ void displayFunc(){
 
 	while (1) {
 
-		std::cout<<std::endl<<"Time | Fuel Consumption | Engine Speed (RPM) | Engine Coolant Temperature | Current Gear | Vehicle Speed"<<std::endl;
+		std::cout<<std::endl;
+		        std::cout<<"Time "
+		                " | Fuel Consumption[" << appcore::readSharedMemInt(0) <<
+		                "] | Engine Speed (RPM)[" << appcore::readSharedMemInt(1) <<
+		                "] | Engine Coolant Temperature["<< appcore::readSharedMemInt(2) <<
+		                "] | Current Gear["<< appcore::readSharedMemInt(3) <<
+		                "] | Vehicle Speed["<< appcore::readSharedMemInt(4) <<"]"<< std::endl;
 		for(int i = 0; i < 15; i++){
 			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 			std::cout<<appcore::getElapsedTimeSeconds() << "\t" << appcore::readSharedMemFloat(5)<<"\t \t \t"<<appcore::readSharedMemFloat(6)<<"\t \t"<<appcore::readSharedMemFloat(7)<<"\t \t \t \t"<<appcore::readSharedMemFloat(8)<<"\t \t"<<appcore::readSharedMemFloat(9)<<std::endl;
